806a761dcc892f2a16d3b5027a244453b706f79b
utkr07 <sutkarsh0719@gmail.com>
2022-10-11 19:02:02 +0530

    Factorial using while loop

--- factorial while loop.py
+++ factorial while loop.py
@@ -1,4 +1,9 @@
-num= int(input("Enter the number for calculating factorial: "))
-fact=1
-i=1
-   
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
+
+n=int(input("Enter the number you want the factorial for: "))
+print(factorial(n))
